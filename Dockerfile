0135c203da0fee65e1249de830a615892e0bb599
Vinh Ngu <vinh-ngu@hotmail.com>
2019-11-01 19:49:17 +0100

    Add initial files.

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM alpine:latest
+
+MAINTAINER vinh-ngu@hotmail.com
+
+ENV RANCHER_VERSION v2.3.0-rc6
+ENV TERM konsole
+
+# Install deps
+RUN apk add wget curl bash ncurses
+
+# Install rancher-cli
+RUN wget -O rancher-cli.tar.gz https://github.com/rancher/cli/releases/download/${RANCHER_VERSION}/rancher-linux-amd64-${RANCHER_VERSION}.tar.gz
+RUN mkdir rancher-cli
+RUN tar xvf rancher-cli.tar.gz -C rancher-cli --strip-components 2
+RUN rm -rf rancher-cli.tar.gz
+RUN ln -s /rancher-cli/rancher /usr/bin/rancher
+
+# Install kubectl
+RUN curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+RUN chmod +x kubectl
+RUN ln -s /kubectl /usr/bin/kubectl
+
+COPY entrypoint.sh entrypoint.sh
+RUN chmod +x entrypoint.sh
+
+WORKDIR /app
+CMD ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+function  logInfo() {
+    color=`tput setaf 2`
+    echo -e $2 "[ ${color}Info$(tput sgr0) ]    $(date "+%D-%T")\t $(tput bold)$1$(tput sgr0)"
+}
+
+function  logError() {
+    color=`tput setaf 1`
+    echo -e $2 "[ ${color}Error$(tput sgr0) ]   $(date "+%D-%T")\t $(tput bold)$1$(tput sgr0)"
+}
+
+function  logWarn() {
+    color=`tput setaf 3`
+    echo -e $2 "[ ${color}Warning$(tput sgr0) ] $(date "+%D-%T")\t $(tput bold)$1$(tput sgr0)"
+}
+
+
+# ==  Rancher login ==
+# Check envs
+logInfo "Check environment variables..."
+to_track=( PLUGIN_RANCHER_URL PLUGIN_RANCHER_TOKEN KUBERNETES_DEPLOYMENT KUBERNETES_NAMESPACE STAMP )
+for env in "${to_track[@]}"; do
+    if [ -z "$env" ]; then
+       	logError "Please set a value for $env"
+	exit -1
+    fi
+done
+logInfo "Login to kubernetes cluster..."
+rancher login $PLUGIN_RANCHER_URL --token $PLUGIN_RANCHER_TOKEN
+
+
+# == Deployment ==
+logInfo "Upgrade $KUBERNETES_DEPLOYMENT."
+rancher kubectl set env deployments/$KUBERNETES_DEPLOYMENT -n $KUBERNETES_NAMESPACE GIT_HASH=$STAMP
+rancher kubectl rollout status deployments/$KUBERNETES_DEPLOYMENT -n $KUBERNETES_NAMESPACE -w
+logInfo "Upgrade succeeded."
