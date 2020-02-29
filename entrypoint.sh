746e11a655f23ba4ef830c763a69adec5d8f9ec6
Vinh Ngu <vinh-ngu@hotmail.com>
2020-02-29 02:12:03 +0100

    Add option to trigger multiple deployments.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 function  logInfo() {
@@ -17,17 +16,22 @@ function  logWarn() {
     echo -e $2 "[ ${color}Warning$(tput sgr0) ] $(date "+%D-%T")\t $(tput bold)$1$(tput sgr0)"
 }
 
-
-# ==  Rancher login ==
 # Check envs
 logInfo "Check environment variables..."
 to_track=( PLUGIN_RANCHER_URL PLUGIN_RANCHER_TOKEN PLUGIN_KUBERNETES_DEPLOYMENT PLUGIN_KUBERNETES_NAMESPACE PLUGIN_STAMP )
 for env in "${to_track[@]}"; do
-    if [ -z "$env" ]; then
+    if [ "${!env}" = "" ]; then
        	logError "Please set a value for $env"
 	exit -1
     fi
 done
+
+# Adapter for https://github.com/Quving/rancher-cicd/blob/master/entrypoint.sh
+RANCHER_URL=$PLUGIN_RANCHER_URL
+RANCHER_TOKEN=$PLUGIN_RANCHER_TOKEN
+KUBERNETES_DEPLOYMENT=$PLUGIN_KUBERNETES_DEPLOYMENT
+KUBERNETES_NAMESPACE=$PLUGIN_KUBERNETES_NAMESPACE
+STAMP=$PLUGIN_STAMP
 
 if [ "${PLUGIN_RANCHER_CONTEXT}" = "" ]; then
     RANCHER_OPTIONS=""
@@ -35,11 +39,32 @@ else
     RANCHER_OPTIONS="--context $PLUGIN_RANCHER_CONTEXT"
 fi
 
+# Rancher login
 logInfo "Login to kubernetes cluster..."
-rancher login $PLUGIN_RANCHER_URL --token $PLUGIN_RANCHER_TOKEN $RANCHER_OPTIONS
+rancher login $RANCHER_URL --token $RANCHER_TOKEN $RANCHER_OPTIONS > /dev/null 2>&1
 
-# == Deployment ==
-logInfo "Upgrade $PLUGIN_KUBERNETES_DEPLOYMENT."
-rancher kubectl set env deployments/$PLUGIN_KUBERNETES_DEPLOYMENT -n $PLUGIN_KUBERNETES_NAMESPACE GIT_HASH=$PLUGIN_STAMP
-rancher kubectl rollout status deployments/$PLUGIN_KUBERNETES_DEPLOYMENT -n $PLUGIN_KUBERNETES_NAMESPACE -w
+# If login failed.
+if [ ! "$(echo $?)" == 0 ]; then
+    logError "Wrong credentials provided. Check your 'RANCHER_URL' and 'RANCHER_TOKEN'"
+    exit 1
+fi
+logInfo "Logged in successfully."
+
+# Deploy service
+KUBECTL_OPTIONS=${KUBECTL_OPTIONS:-''}
+IFS=',' # hyphen (-) is set as delimiter
+read -ra ADDR <<< "$KUBERNETES_DEPLOYMENT" # str is read into an array as tokens separated by IFS
+for workload in "${ADDR[@]}"; do # access each element of array
+    logInfo "Upgrade $workload..."
+    rancher kubectl $KUBECTL_OPTIONS set env deployments/$workload -n $KUBERNETES_NAMESPACE GIT_HASH=$STAMP > error.log 2>&1
+
+    # If upgrade failed.
+    if [ ! "$(echo $?)" == 0 ]; then
+        logError "Error occured while upgrading k8s deployment ($workload). Please check the logs below."
+        cat error.log
+        exit 1
+    fi
+    rancher kubectl $KUBECTL_OPTIONS rollout status deployments/$workload -n $KUBERNETES_NAMESPACE -w
+done
 logInfo "Upgrade succeeded."
+
